4d335e47c16b3a003970c4b3c600128dca638fff
lucacambi@rentalcars.com <lucacambi@rentalcars.com>
2020-10-23 10:53:12 +0100

    adds banner

--- src/main/java/com/cambi/claranet/ClaranetApplication.java
+++ src/main/java/com/cambi/claranet/ClaranetApplication.java
@@ -22,6 +22,7 @@ public class ClaranetApplication implements CommandLineRunner {
 
   @Override
   public void run(String... args) {
+    initialBanner();
 
     boolean isExitCommand = false;
 
@@ -31,5 +32,27 @@ public class ClaranetApplication implements CommandLineRunner {
       Command command = commandExecutor.execute(input);
       isExitCommand = command.isExitCommand();
     }
+  }
+
+  private void initialBanner() {
+    System.out.println(" ");
+    System.out.println(" ");
+    System.out.println("Welcome to Claranet Social Network demo!");
+    System.out.println(" ");
+    System.out.println("MyUserName -> Hello World!");
+    System.out.println( "... add a post to your time line");
+    System.out.println("MyUserName follow AnotherUser");
+    System.out.println("... to follow another user");
+    System.out.println("MyUserName");
+    System.out.println("... to see your posts");
+    System.out.println("MyUserName wall");
+    System.out.println("... to see yours and your following users posts ");
+    System.out.println(" ");
+    System.out.println("For the purpose of this demo only alphanumeric users names are allowed");
+    System.out.println(" ");
+    System.out.println("Type exit to end your session");
+    System.out.println(" ");
+    System.out.println("Enjoy!");
+    System.out.println(" ");
   }
 }
